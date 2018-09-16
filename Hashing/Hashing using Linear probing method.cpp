811ec19ad610afce4ed405d7d4d7cfc9e7918c37
AbirFFA <42368238+AbirFFA@users.noreply.github.com>
2018-09-17 01:19:36 +0600

    Add files via upload

---
+++ Hashing/Hashing using Linear probing method.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+#define SIZE 20
+
+using namespace std;
+
+struct DataItem {
+    int data;
+    int key;
+};
+
+struct DataItem* dataList[SIZE];
+struct DataItem* singleItem;
+struct DataItem* item;
+
+int getHashCode(int key) {
+    return key % SIZE;
+}
+
+struct DataItem *search(int key) {
+    //get the hash
+    int hashIndex = getHashCode(key);
+
+    //move in array until an empty
+    while(dataList[hashIndex] != NULL) {
+        if(dataList[hashIndex]->key == key)
+            return dataList[hashIndex];
+
+        //go to next cell
+        ++hashIndex;
+
+        //wrap around the table
+        hashIndex %= SIZE;
+    }
+    return NULL;
+};
+
+void insert (int key, int data) {
+    struct DataItem *item = (struct DataItem*) malloc(sizeof(struct DataItem));
+    item->data = data;
+    item->key = key;
+
+    //get the hash
+    int hashIndex = getHashCode(key);
+
+    //move in array until an empty or deleted cell
+    while(dataList[hashIndex] != NULL && dataList[hashIndex]->key != -1) {
+        //go to next cell
+        ++hashIndex;
+
+        //wrap around the table
+        hashIndex %= SIZE;
+    }
+
+    dataList[hashIndex] = item;
+
+}
+
+struct DataItem* del(struct DataItem* item) {
+    int key = item->key;
+
+    //get the hash
+    int hashIndex = getHashCode(key);
+
+    //move in array until an empty
+    while(dataList[hashIndex] != NULL) {
+        if(dataList[hashIndex]->key == key) {
+            struct DataItem* temp = dataList[hashIndex];
+
+            //assign a dummy item at deleted position
+            dataList[hashIndex] = singleItem;
+            return temp;
+        }
+        //go to next cell
+        ++hashIndex;
+
+        //wrap around the table
+        hashIndex %= SIZE;
+    }
+    return NULL;
+};
+
+void display() {
+    int i = 0;
+
+    for(i = 0; i < SIZE; i++) {
+
+        if(dataList[i] != NULL)
+            printf(" (%d%d)\n", dataList[i]->key, dataList[i]->data);
+        else
+            printf(" ~~ \n");
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    singleItem = (struct DataItem*) malloc(sizeof(struct DataItem));
+    singleItem->data = -1;
+    singleItem->key = -1;
+
+    insert(1, 20);
+    insert(2, 70);
+    insert(42, 80);
+    insert(4, 25);
+    insert(12, 44);
+    insert(14, 32);
+    insert(17, 11);
+    insert(13, 78);
+    insert(37, 97);
+
+    display();
+
+    item = search(37);
+
+    if(item != NULL)
+        printf("Element found: %d\n", item->data);
+    else
+        printf("Element not found");
+
+    del(item);
+    item = search(37);
+
+    if(item != NULL)
+        printf("Element found: %d\n", item->data);
+    else
+        printf("Element not found\n");
+
+    return 0;
+}
